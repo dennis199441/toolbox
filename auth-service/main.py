66e2070807685860f5badc16f0c9de47560d02c0
Dennis Cheung <dennis.cheung@emali.io>
2021-02-06 22:22:21 +0800

    auth-service: add cors

--- auth-service/main.py
+++ auth-service/main.py
@@ -1,8 +1,10 @@
 from app import create_app
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS, cross_origin
 import config
 
 app = create_app()
+cors = CORS(app)
 jwt = JWTManager(app)
 
 if __name__ == '__main__':
