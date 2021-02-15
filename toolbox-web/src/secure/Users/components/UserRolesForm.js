9d122f9893dbc2abe31da8d22515db7ab16c7f17
Dennis Cheung <dennis199441@gmail.com>
2021-02-15 19:48:58 +0800

    add revoke role button

--- toolbox-web/src/secure/Users/components/UserRolesForm.js
+++ toolbox-web/src/secure/Users/components/UserRolesForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,7 +9,7 @@ import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
 import Title from './Title';
-import { getUserRoles } from '../../../utils';
+import { getUserDetails, getUserRoles, revokeRole } from '../../../utils';
 
 const useStyles = makeStyles({
   table: {
@@ -16,21 +17,35 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, description) {
-  return { name, description };
+function createData(id, name, description) {
+  return { id, name, description };
 }
 
 export default function UserDetailsForm(props) {
 
   const history = useHistory();
-
   const [rows, setRows] = useState([]);
+
+  const handleRevoke = async(e) => {
+    let roleId = e.target.getAttribute('value');
+    let user = await getUserDetails(props.username);
+    await revokeRole(user.id, roleId);
+    async function fetchData() {
+      const data = await getUserRoles(props.username);
+      const roles = data.map((role) => {
+        return createData(role.id, role.name, role.description);
+      })
+
+      setRows(roles);
+    };
+    fetchData();
+  }
 
   useEffect(() => {
     async function fetchData() {
       const data = await getUserRoles(props.username);
       const roles = data.map((role) => {
-        return createData(role.name, role.description);
+        return createData(role.id, role.name, role.description);
       })
 
       setRows(roles);
@@ -52,6 +67,7 @@ export default function UserDetailsForm(props) {
             <TableCell align="left">
               <Typography align="left"><b>Description</b></Typography>
             </TableCell>
+            <TableCell align="right"></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -66,6 +82,13 @@ export default function UserDetailsForm(props) {
                 <Typography align="left">
                   {row.description}
                 </Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Button onClick={handleRevoke} color="secondary">
+                <Typography align="left" value={row.id}>
+                  Revoke
+                </Typography>
+                </Button>
               </TableCell>
             </TableRow>
           ))}
