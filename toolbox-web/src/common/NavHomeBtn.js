654f79b64f1a12787e66946d06f423d34b435b75
Dennis Cheung <dennis199441@gmail.com>
2021-03-19 16:10:18 +0800

    fix home button

--- toolbox-web/src/common/NavHomeBtn.js
+++ toolbox-web/src/common/NavHomeBtn.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
 import logo from '../static/logo.png';
 
 export default function NavHomeBtn() {
-
-    let history = useHistory();
-
-    const handleClick = () => {
-        history.push("/");
-    }
-
     return (
-        <Button onClick={handleClick} color="inherit">
-            <img src={logo} alt="CodingDaily.dev"/>
-        </Button>
+        <Link href="/">
+            <img src={logo} alt="CodingDaily.dev" />
+        </Link>
     )
 }
