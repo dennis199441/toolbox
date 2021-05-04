be3a748041c0f6ce04c5811814b256d12a22bcda
Dennis Cheung <dennis199441@gmail.com>
2021-05-04 19:24:16 +0800

    update

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 3. Cron Job Manager
 
 4. Speech-to-text index
+---
+## Notice
+
+1. (4th May, 2021) Moved `toolbox-web` to `dennis199441/toolbox-web` (https://dennis199441.github.io/toolbox-web/#/)
--- toolbox-web/src/common/index.js
+++
@@ -1,6 +0,0 @@
-import NavBar from './NavBar';
-import SecureRoute from './SecureRoute';
-import PublicRoute from './PublicRoute';
-import NotFound from './NotFound';
-
-export { NavBar, SecureRoute, PublicRoute, NotFound };
